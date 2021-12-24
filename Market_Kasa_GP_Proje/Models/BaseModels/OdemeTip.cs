fa7d3440f09955d31b15119fd5e8b42c01cdee54
oktaygulec <oktay.gulec@outlook.com>
2021-12-24 15:02:34 +0300

    Added models

---
+++ Market_Kasa_GP_Proje/Models/BaseModels/OdemeTip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Kasa_GP_Proje.Models
+{
+    public class OdemeTip : IModel
+    {
+        public int Id { get; set; }
+        public string OdemeTipAd { get; set; }
+
+        public List<SqlParameter> GetInsertParameters()
+        {
+            return new List<SqlParameter> {
+                new SqlParameter("OdemeTipAd", this.OdemeTipAd)
+            };
+        }
+
+        public List<SqlParameter> GetUpdateParameters()
+        {
+            List<SqlParameter> parameters = GetInsertParameters();
+            parameters.Add(new SqlParameter("OdemeTipId", this.Id));
+            return parameters;
+        }
+
+        public void ReadItem(SqlDataReader reader)
+        {
+            this.Id = Convert.ToInt32(reader["OdemeTipId"]);
+            this.OdemeTipAd = reader["OdemeTipAd"].ToString();
+        }
+    }
+}
