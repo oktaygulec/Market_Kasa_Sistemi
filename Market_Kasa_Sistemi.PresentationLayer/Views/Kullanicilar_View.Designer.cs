68c1280b950dc6821c9dd9d6275864b4253dd628
Samet ÖZTÜRK <hyuna.akali@gmail.com>
2021-12-29 00:55:05 +0300

    Kullanicilar_View eklendi

---
+++ Market_Kasa_Sistemi.PresentationLayer/Views/Kullanicilar_View.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Market_Kasa_Sistemi.Views
+{
+    partial class Kullanicilar_View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kullaniciEkleButton = new System.Windows.Forms.Button();
+            this.cikisButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuLine = new System.Windows.Forms.Label();
+            this.kullaniciAdiTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.kullaniciSifreTxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.personelIdTxt = new System.Windows.Forms.TextBox();
+            this.kullaniciSilButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kullaniciEkleButton
+            // 
+            this.kullaniciEkleButton.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.kullaniciEkleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kullaniciEkleButton.FlatAppearance.BorderSize = 0;
+            this.kullaniciEkleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kullaniciEkleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kullaniciEkleButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.kullaniciEkleButton.Location = new System.Drawing.Point(909, 244);
+            this.kullaniciEkleButton.Margin = new System.Windows.Forms.Padding(0);
+            this.kullaniciEkleButton.Name = "kullaniciEkleButton";
+            this.kullaniciEkleButton.Size = new System.Drawing.Size(277, 38);
+            this.kullaniciEkleButton.TabIndex = 16;
+            this.kullaniciEkleButton.Text = "KULLANICI EKLE";
+            this.kullaniciEkleButton.UseVisualStyleBackColor = false;
+            // 
+            // cikisButton
+            // 
+            this.cikisButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.cikisButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cikisButton.FlatAppearance.BorderSize = 0;
+            this.cikisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cikisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cikisButton.Location = new System.Drawing.Point(952, 488);
+            this.cikisButton.Margin = new System.Windows.Forms.Padding(0);
+            this.cikisButton.Name = "cikisButton";
+            this.cikisButton.Size = new System.Drawing.Size(215, 123);
+            this.cikisButton.TabIndex = 15;
+            this.cikisButton.Text = "ÇIKIŞ YAP";
+            this.cikisButton.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 56);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 555);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(146, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 25);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Kullanıcı Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(442, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Şifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(671, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 25);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Personel ID";
+            // 
+            // menuLine
+            // 
+            this.menuLine.AutoSize = true;
+            this.menuLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuLine.Location = new System.Drawing.Point(10, 32);
+            this.menuLine.Name = "menuLine";
+            this.menuLine.Size = new System.Drawing.Size(882, 16);
+            this.menuLine.TabIndex = 22;
+            this.menuLine.Text = "_________________________________________________________________________________" +
+    "____________________________________________";
+            // 
+            // kullaniciAdiTxt
+            // 
+            this.kullaniciAdiTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kullaniciAdiTxt.Location = new System.Drawing.Point(909, 78);
+            this.kullaniciAdiTxt.Name = "kullaniciAdiTxt";
+            this.kullaniciAdiTxt.Size = new System.Drawing.Size(277, 32);
+            this.kullaniciAdiTxt.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(904, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 25);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Kullanıcı Ekle";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(906, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(280, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "_______________________________________";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(909, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Kullanıcı Adı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(909, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 16);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Şifre";
+            // 
+            // kullaniciSifreTxt
+            // 
+            this.kullaniciSifreTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kullaniciSifreTxt.Location = new System.Drawing.Point(909, 137);
+            this.kullaniciSifreTxt.Name = "kullaniciSifreTxt";
+            this.kullaniciSifreTxt.Size = new System.Drawing.Size(277, 32);
+            this.kullaniciSifreTxt.TabIndex = 27;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(909, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 16);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Personel ID";
+            // 
+            // personelIdTxt
+            // 
+            this.personelIdTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelIdTxt.Location = new System.Drawing.Point(909, 200);
+            this.personelIdTxt.Name = "personelIdTxt";
+            this.personelIdTxt.Size = new System.Drawing.Size(277, 32);
+            this.personelIdTxt.TabIndex = 29;
+            // 
+            // kullaniciSilButton
+            // 
+            this.kullaniciSilButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.kullaniciSilButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kullaniciSilButton.FlatAppearance.BorderSize = 0;
+            this.kullaniciSilButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kullaniciSilButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kullaniciSilButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.kullaniciSilButton.Location = new System.Drawing.Point(909, 296);
+            this.kullaniciSilButton.Margin = new System.Windows.Forms.Padding(0);
+            this.kullaniciSilButton.Name = "kullaniciSilButton";
+            this.kullaniciSilButton.Size = new System.Drawing.Size(277, 38);
+            this.kullaniciSilButton.TabIndex = 31;
+            this.kullaniciSilButton.Text = "KULLANICI SİL";
+            this.kullaniciSilButton.UseVisualStyleBackColor = false;
+            // 
+            // Kullanicilar_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 623);
+            this.Controls.Add(this.kullaniciSilButton);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.personelIdTxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.kullaniciSifreTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.kullaniciAdiTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.kullaniciEkleButton);
+            this.Controls.Add(this.cikisButton);
+            this.Controls.Add(this.menuLine);
+            this.Name = "Kullanicilar_View";
+            this.Text = "Kullanıcılar";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kullaniciEkleButton;
+        private System.Windows.Forms.Button cikisButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label menuLine;
+        private System.Windows.Forms.TextBox kullaniciAdiTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox kullaniciSifreTxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox personelIdTxt;
+        private System.Windows.Forms.Button kullaniciSilButton;
+    }
+}
