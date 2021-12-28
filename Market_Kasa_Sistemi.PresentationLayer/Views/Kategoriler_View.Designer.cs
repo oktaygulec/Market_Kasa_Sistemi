d3a71d635968ac89e890d69a57c94acf1ff4ff11
Samet ÖZTÜRK <hyuna.akali@gmail.com>
2021-12-29 01:51:19 +0300

    Kategoriler_View Eklendi & Data Grid Viewlerin nameleri [element_adı]dataGrid yerine [eleman_adı]DGW olarak değiştirildi

---
+++ Market_Kasa_Sistemi.PresentationLayer/Views/Kategoriler_View.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Market_Kasa_Sistemi.Views
+{
+    partial class Kategoriler_View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kategoriSilButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.kategoriAdiTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kategorilerDGW = new System.Windows.Forms.DataGridView();
+            this.kategoriEkleButton = new System.Windows.Forms.Button();
+            this.cikisButton = new System.Windows.Forms.Button();
+            this.menuLine = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kategorilerDGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kategoriSilButton
+            // 
+            this.kategoriSilButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.kategoriSilButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kategoriSilButton.FlatAppearance.BorderSize = 0;
+            this.kategoriSilButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kategoriSilButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kategoriSilButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.kategoriSilButton.Location = new System.Drawing.Point(363, 193);
+            this.kategoriSilButton.Margin = new System.Windows.Forms.Padding(0);
+            this.kategoriSilButton.Name = "kategoriSilButton";
+            this.kategoriSilButton.Size = new System.Drawing.Size(277, 38);
+            this.kategoriSilButton.TabIndex = 59;
+            this.kategoriSilButton.Text = "KATEGORİ SİL";
+            this.kategoriSilButton.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(363, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Kategori Adı";
+            // 
+            // kategoriAdiTxt
+            // 
+            this.kategoriAdiTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kategoriAdiTxt.Location = new System.Drawing.Point(363, 83);
+            this.kategoriAdiTxt.Name = "kategoriAdiTxt";
+            this.kategoriAdiTxt.Size = new System.Drawing.Size(277, 32);
+            this.kategoriAdiTxt.TabIndex = 57;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(358, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 25);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Kategori Ekle";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(360, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(280, 16);
+            this.label7.TabIndex = 56;
+            this.label7.Text = "_______________________________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(183, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Yetki Adı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "ID";
+            // 
+            // kategorilerDGW
+            // 
+            this.kategorilerDGW.AllowUserToAddRows = false;
+            this.kategorilerDGW.AllowUserToDeleteRows = false;
+            this.kategorilerDGW.AllowUserToResizeColumns = false;
+            this.kategorilerDGW.AllowUserToResizeRows = false;
+            this.kategorilerDGW.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.kategorilerDGW.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.kategorilerDGW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.kategorilerDGW.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.kategorilerDGW.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.kategorilerDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.kategorilerDGW.ColumnHeadersVisible = false;
+            this.kategorilerDGW.Location = new System.Drawing.Point(12, 61);
+            this.kategorilerDGW.MultiSelect = false;
+            this.kategorilerDGW.Name = "kategorilerDGW";
+            this.kategorilerDGW.ReadOnly = true;
+            this.kategorilerDGW.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.kategorilerDGW.RowHeadersVisible = false;
+            this.kategorilerDGW.RowHeadersWidth = 51;
+            this.kategorilerDGW.RowTemplate.Height = 24;
+            this.kategorilerDGW.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.kategorilerDGW.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.kategorilerDGW.Size = new System.Drawing.Size(304, 578);
+            this.kategorilerDGW.TabIndex = 51;
+            // 
+            // kategoriEkleButton
+            // 
+            this.kategoriEkleButton.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.kategoriEkleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kategoriEkleButton.FlatAppearance.BorderSize = 0;
+            this.kategoriEkleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kategoriEkleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kategoriEkleButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.kategoriEkleButton.Location = new System.Drawing.Point(363, 141);
+            this.kategoriEkleButton.Margin = new System.Windows.Forms.Padding(0);
+            this.kategoriEkleButton.Name = "kategoriEkleButton";
+            this.kategoriEkleButton.Size = new System.Drawing.Size(277, 38);
+            this.kategoriEkleButton.TabIndex = 50;
+            this.kategoriEkleButton.Text = "KATEGORİ EKLE";
+            this.kategoriEkleButton.UseVisualStyleBackColor = false;
+            // 
+            // cikisButton
+            // 
+            this.cikisButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.cikisButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cikisButton.FlatAppearance.BorderSize = 0;
+            this.cikisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cikisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cikisButton.Location = new System.Drawing.Point(425, 516);
+            this.cikisButton.Margin = new System.Windows.Forms.Padding(0);
+            this.cikisButton.Name = "cikisButton";
+            this.cikisButton.Size = new System.Drawing.Size(215, 123);
+            this.cikisButton.TabIndex = 49;
+            this.cikisButton.Text = "ÇIKIŞ YAP";
+            this.cikisButton.UseVisualStyleBackColor = false;
+            // 
+            // menuLine
+            // 
+            this.menuLine.AutoSize = true;
+            this.menuLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuLine.Location = new System.Drawing.Point(9, 37);
+            this.menuLine.Name = "menuLine";
+            this.menuLine.Size = new System.Drawing.Size(308, 16);
+            this.menuLine.TabIndex = 54;
+            this.menuLine.Text = "___________________________________________";
+            // 
+            // Kategoriler_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 656);
+            this.Controls.Add(this.kategoriSilButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.kategoriAdiTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kategorilerDGW);
+            this.Controls.Add(this.kategoriEkleButton);
+            this.Controls.Add(this.cikisButton);
+            this.Controls.Add(this.menuLine);
+            this.Name = "Kategoriler_View";
+            this.Text = "Kategoriler";
+            ((System.ComponentModel.ISupportInitialize)(this.kategorilerDGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kategoriSilButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox kategoriAdiTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView kategorilerDGW;
+        private System.Windows.Forms.Button kategoriEkleButton;
+        private System.Windows.Forms.Button cikisButton;
+        private System.Windows.Forms.Label menuLine;
+    }
+}
