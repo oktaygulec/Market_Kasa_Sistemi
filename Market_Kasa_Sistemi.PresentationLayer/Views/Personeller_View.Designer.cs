6470fd6928067233159f648e9194876c984ea2c4
Samet ÖZTÜRK <hyuna.akali@gmail.com>
2021-12-29 02:27:58 +0300

    Personeller_View eklendi

---
+++ Market_Kasa_Sistemi.PresentationLayer/Views/Personeller_View.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Market_Kasa_Sistemi.Views
+{
+    partial class Personeller_View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.personellerDGW = new System.Windows.Forms.DataGridView();
+            this.cikisYapButton = new System.Windows.Forms.Button();
+            this.personelEkleButton = new System.Windows.Forms.Button();
+            this.PersonelAdiTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuLine = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.personelSoyadiTxt = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.personelSilButton = new System.Windows.Forms.Button();
+            this.personelDuzenleButton = new System.Windows.Forms.Button();
+            this.personelTipiComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.personellerDGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // personellerDGW
+            // 
+            this.personellerDGW.AllowUserToAddRows = false;
+            this.personellerDGW.AllowUserToDeleteRows = false;
+            this.personellerDGW.AllowUserToResizeColumns = false;
+            this.personellerDGW.AllowUserToResizeRows = false;
+            this.personellerDGW.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.personellerDGW.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.personellerDGW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.personellerDGW.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.personellerDGW.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.personellerDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.personellerDGW.ColumnHeadersVisible = false;
+            this.personellerDGW.Location = new System.Drawing.Point(14, 128);
+            this.personellerDGW.MultiSelect = false;
+            this.personellerDGW.Name = "personellerDGW";
+            this.personellerDGW.ReadOnly = true;
+            this.personellerDGW.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.personellerDGW.RowHeadersVisible = false;
+            this.personellerDGW.RowHeadersWidth = 51;
+            this.personellerDGW.RowTemplate.Height = 24;
+            this.personellerDGW.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.personellerDGW.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.personellerDGW.Size = new System.Drawing.Size(1201, 677);
+            this.personellerDGW.TabIndex = 28;
+            // 
+            // cikisYapButton
+            // 
+            this.cikisYapButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.cikisYapButton.FlatAppearance.BorderSize = 0;
+            this.cikisYapButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cikisYapButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisYapButton.Location = new System.Drawing.Point(1246, 699);
+            this.cikisYapButton.Name = "cikisYapButton";
+            this.cikisYapButton.Size = new System.Drawing.Size(277, 106);
+            this.cikisYapButton.TabIndex = 39;
+            this.cikisYapButton.Text = "ÇIKIŞ YAP";
+            this.cikisYapButton.UseVisualStyleBackColor = false;
+            // 
+            // personelEkleButton
+            // 
+            this.personelEkleButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(255)))), ((int)(((byte)(130)))));
+            this.personelEkleButton.FlatAppearance.BorderSize = 0;
+            this.personelEkleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.personelEkleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelEkleButton.Location = new System.Drawing.Point(1246, 326);
+            this.personelEkleButton.Name = "personelEkleButton";
+            this.personelEkleButton.Size = new System.Drawing.Size(277, 38);
+            this.personelEkleButton.TabIndex = 38;
+            this.personelEkleButton.Text = "PERSONEL EKLE";
+            this.personelEkleButton.UseVisualStyleBackColor = false;
+            // 
+            // PersonelAdiTxt
+            // 
+            this.PersonelAdiTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.PersonelAdiTxt.Location = new System.Drawing.Point(1246, 146);
+            this.PersonelAdiTxt.Name = "PersonelAdiTxt";
+            this.PersonelAdiTxt.Size = new System.Drawing.Size(277, 32);
+            this.PersonelAdiTxt.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(1241, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 25);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Personel Girişi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(447, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 25);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Personel Soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(752, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 25);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Başlangıç Tarihi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(177, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 25);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Personel Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "ID";
+            // 
+            // menuLine
+            // 
+            this.menuLine.AutoSize = true;
+            this.menuLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuLine.Location = new System.Drawing.Point(11, 109);
+            this.menuLine.Name = "menuLine";
+            this.menuLine.Size = new System.Drawing.Size(1204, 16);
+            this.menuLine.TabIndex = 34;
+            this.menuLine.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__________";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(749, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 46);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "PERSONELLER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1243, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(280, 16);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "_______________________________________";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(1057, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 25);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Personel Tipi";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1246, 128);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 16);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "Personel Adı";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1246, 186);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(107, 16);
+            this.label11.TabIndex = 44;
+            this.label11.Text = "Personel Soyadı";
+            // 
+            // personelSoyadiTxt
+            // 
+            this.personelSoyadiTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelSoyadiTxt.Location = new System.Drawing.Point(1246, 204);
+            this.personelSoyadiTxt.Name = "personelSoyadiTxt";
+            this.personelSoyadiTxt.Size = new System.Drawing.Size(277, 32);
+            this.personelSoyadiTxt.TabIndex = 43;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(1246, 248);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 16);
+            this.label12.TabIndex = 46;
+            this.label12.Text = "Personel Tipi";
+            // 
+            // personelSilButton
+            // 
+            this.personelSilButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.personelSilButton.FlatAppearance.BorderSize = 0;
+            this.personelSilButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.personelSilButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelSilButton.Location = new System.Drawing.Point(1246, 370);
+            this.personelSilButton.Name = "personelSilButton";
+            this.personelSilButton.Size = new System.Drawing.Size(277, 38);
+            this.personelSilButton.TabIndex = 51;
+            this.personelSilButton.Text = "PERSONEL SİL";
+            this.personelSilButton.UseVisualStyleBackColor = false;
+            // 
+            // personelDuzenleButton
+            // 
+            this.personelDuzenleButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(199)))));
+            this.personelDuzenleButton.FlatAppearance.BorderSize = 0;
+            this.personelDuzenleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.personelDuzenleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelDuzenleButton.Location = new System.Drawing.Point(1246, 470);
+            this.personelDuzenleButton.Name = "personelDuzenleButton";
+            this.personelDuzenleButton.Size = new System.Drawing.Size(277, 38);
+            this.personelDuzenleButton.TabIndex = 52;
+            this.personelDuzenleButton.Text = "PERSONEL DÜZENLE";
+            this.personelDuzenleButton.UseVisualStyleBackColor = false;
+            // 
+            // personelTipiComboBox
+            // 
+            this.personelTipiComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.personelTipiComboBox.FormattingEnabled = true;
+            this.personelTipiComboBox.Location = new System.Drawing.Point(1249, 267);
+            this.personelTipiComboBox.Name = "personelTipiComboBox";
+            this.personelTipiComboBox.Size = new System.Drawing.Size(277, 34);
+            this.personelTipiComboBox.TabIndex = 53;
+            // 
+            // Personeller_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1535, 817);
+            this.Controls.Add(this.personelTipiComboBox);
+            this.Controls.Add(this.personelDuzenleButton);
+            this.Controls.Add(this.personelSilButton);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.personelSoyadiTxt);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.personellerDGW);
+            this.Controls.Add(this.cikisYapButton);
+            this.Controls.Add(this.personelEkleButton);
+            this.Controls.Add(this.PersonelAdiTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuLine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Name = "Personeller_View";
+            this.Text = "Personeller";
+            ((System.ComponentModel.ISupportInitialize)(this.personellerDGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView personellerDGW;
+        private System.Windows.Forms.Button cikisYapButton;
+        private System.Windows.Forms.Button personelEkleButton;
+        private System.Windows.Forms.TextBox PersonelAdiTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label menuLine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox personelSoyadiTxt;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button personelSilButton;
+        private System.Windows.Forms.Button personelDuzenleButton;
+        private System.Windows.Forms.ComboBox personelTipiComboBox;
+    }
+}
