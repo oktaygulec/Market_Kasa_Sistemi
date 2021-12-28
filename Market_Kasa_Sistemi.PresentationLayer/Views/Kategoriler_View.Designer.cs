2e04ec8f484df8b1a4deff01b7f6bbb8542d205d
Samet ÖZTÜRK <hyuna.akali@gmail.com>
2021-12-29 01:53:08 +0300

    Kategoriler_View'da label'ın texti düzenlendi

--- Market_Kasa_Sistemi.PresentationLayer/Views/Kategoriler_View.Designer.cs
+++ Market_Kasa_Sistemi.PresentationLayer/Views/Kategoriler_View.Designer.cs
@@ -100,9 +100,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(183, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.Size = new System.Drawing.Size(131, 25);
             this.label2.TabIndex = 53;
-            this.label2.Text = "Yetki Adı";
+            this.label2.Text = "Kategori Adı";
             // 
             // label1
             // 
