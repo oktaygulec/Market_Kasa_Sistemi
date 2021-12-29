074695c802d6b9849c71b26fd88f7d2047c4e2c6
oktaygulec <oktay.gulec@outlook.com>
2021-12-29 22:38:07 +0300

    Added repositories and uow

---
+++ Market_Kasa_Sistemi.DatabaseAccessLayer/Repositories/KategoriRepository.cs
@@ -0,0 +1,56 @@
+﻿using Market_Kasa_Sistemi.DatabaseAccessLayer.DatabaseContext;
+using Market_Kasa_Sistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Kasa_Sistemi.DatabaseAccessLayer.Repositories
+{
+    public class KategoriRepository : ARepository<Kategori>
+    {
+        public KategoriRepository(DBContext context) : base(context) {}
+
+        public override object Add(Kategori item)
+        {
+            using (SqlCommand cmd = context.CreateCommand("SPKategoriAdd", item.GetInsertParameters()))
+            {
+                return context.ExecuteScalar(cmd);
+            }
+        }
+
+        public override Kategori GetItem(object value)
+        {
+            using (SqlCommand cmd = context.CreateCommand("SPKategoriGetById", new SqlParameter("@KategoriId", value)))
+            {
+                return context.GetItem<Kategori>(cmd);
+            }
+        }
+
+        public override int Remove(Kategori item)
+        {
+            using (SqlCommand cmd = context.CreateCommand("SPKategoriDelete", item.GetIdParameter()))
+            {
+                return context.ExecuteNonQuery(cmd);
+            }
+        }
+
+        public override List<Kategori> ToList()
+        {
+            using (SqlCommand cmd = context.CreateCommand("SPKategoriGetAll"))
+            {
+                return context.ToList<Kategori>(cmd);
+            }
+        }
+
+        public override int Update(Kategori item)
+        {
+            using (SqlCommand cmd = context.CreateCommand("SPKategoriUpdate", item.GetUpdateParameters()))
+            {
+                return context.ExecuteNonQuery(cmd);
+            }
+        }
+    }
+}
